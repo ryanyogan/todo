27aac8521356afe7116467f0c3980274d12552e6
Ryan Yogan <ryanyogan@gmail.com>
2015-04-12 01:30:01 -0500

    Created the initiall app bootstrapper

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-var React = require('react-native');
+var styles            = require('./application/styles/styles');
+var TodoListContainer = require('./application/components/TodoListContainer');
+var React             = require('react-native');
+
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  NavigatorIOS,
+  Component
 } = React;
 
-var todo = React.createClass({
-  render: function() {
+class TodoApp extend Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={style.navigator}
+        initialRoute={{component: TodoListContainer, title: 'To Do\'s'}} />
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('todo', () => todo);
+AppRegistry.registerComponet('TodoApp', () => TodoApp);
